9ab21ec8cc9628fa6d82f6e5e045ec2d37bce677
Ajay Maharjan <maharjanajay0007@gmail.com>
2023-05-25 21:12:51 +0545

    spring security 2023

---
+++ src/main/java/com/springsecuirty2023/SpringSecuirty2023Application.java
@@ -0,0 +1,13 @@
+package com.springsecuirty2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuirty2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuirty2023Application.class, args);
+	}
+
+}
